cf9d027ed3c26687b773d07dbfeceac1d7431107
Dragomir Dragnev <60980007+drago333@users.noreply.github.com>
2023-04-23 08:21:50 +0300

    Add some sample structure

---
+++ navigation/AppNavigator.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+
+import HomeScreen from '../screens/HomeScreen';
+import DetailScreen from '../screens/DetailScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+
+const Stack = createStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Detail" component={DetailScreen} />
+        <Stack.Screen name="Settings" component={SettingsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
---
+++ screens/SettingsScreen.tsx
@@ -0,0 +1,62 @@
+import React, {useState} from 'react';
+import {View, Text, Switch, StyleSheet} from 'react-native';
+
+const SettingsScreen = () => {
+  const [notificationsEnabled, setNotificationsEnabled] = useState(false);
+  const [darkModeEnabled, setDarkModeEnabled] = useState(false);
+
+  const handleNotificationsToggle = () => {
+    setNotificationsEnabled(!notificationsEnabled);
+  };
+
+  const handleDarkModeToggle = () => {
+    setDarkModeEnabled(!darkModeEnabled);
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Notifications</Text>
+        <Switch
+          value={notificationsEnabled}
+          onValueChange={handleNotificationsToggle}
+        />
+      </View>
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Appearance</Text>
+        <View style={styles.appearanceSection}>
+          <Text style={styles.appearanceOption}>Dark mode</Text>
+          <Switch value={darkModeEnabled} onValueChange={handleDarkModeToggle} />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingHorizontal: 16,
+    paddingVertical: 24,
+  },
+  section: {
+    marginBottom: 16,
+  },
+  sectionTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  appearanceSection: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  appearanceOption: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
+});
+
+export default SettingsScreen;
