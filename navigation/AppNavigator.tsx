00313a8fa4148602d0567c3850577fff72c7b25a
Dragomir Dragnev <60980007+drago333@users.noreply.github.com>
2023-06-01 13:09:26 +0300

    Add icons to tab navigation

--- navigation/AppNavigator.tsx
+++ navigation/AppNavigator.tsx
@@ -9,6 +9,7 @@ import AgendaScreen from '../screens/AgendaScreen';
 import MonthlyScreen from '../screens/MonthlyScreen';
 import WeeklyScreen from '../screens/WeeklyScreen';
 import UploadDataScreen from '../screens/UploadDataScreen';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export const SCREENS = {
   AGENDA_SCREEN: 'AgendaScreen',
@@ -59,11 +60,52 @@ const SettingsStack = () => {
 };
 
 const AppNavigator = () => {
+  const getTabBarIcon = (route: any) => ({
+    tabBarIcon: ({
+      focused,
+      color,
+      size,
+    }: {
+      focused: boolean;
+      color: string;
+      size: number;
+    }) => {
+      let iconName = '';
+      switch (route.name) {
+        case 'Agenda':
+          iconName = 'ios-list';
+          break;
+        case 'Weekly View':
+          iconName = 'ios-albums';
+          break;
+        case 'Monthly':
+          iconName = 'ios-calendar';
+          break;
+        case 'Settings':
+          iconName = 'ios-settings';
+          break;
+        default:
+          iconName = 'ios-calendar';
+          break;
+      }
+      if (!focused) {
+        iconName = `${iconName}-outline`;
+      }
+      return <Ionicons name={iconName} size={size} color={color} />;
+    },
+    tabBarActiveTintColor: 'tomato',
+    tabBarInactiveTintColor: 'gray',
+  });
+
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator screenOptions={({route}) => getTabBarIcon(route)}>
         <Tab.Screen name="Agenda" component={AgendaStack} />
-        <Tab.Screen name="Weekly" component={WeeklyStack} />
+        <Tab.Screen
+          name="Weekly View"
+          options={{tabBarLabel: 'Weekly'}}
+          component={WeeklyStack}
+        />
         <Tab.Screen name="Monthly" component={MonthlyStack} />
         <Tab.Screen name="Settings" component={SettingsStack} />
       </Tab.Navigator>
